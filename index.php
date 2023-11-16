5f466717183343a15f9bc816d95f7c089bb1b444
SAAKS6 <asfandyarnaeem81@gmail.com>
2023-11-16 16:41:58 +0500

    INITIATION

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RIHAL</title>
+</head>
+<body>
+    <h1>HOW ARE YOU BRO?</h1>
+    <h2>I AM GOOOOD, WHAT AbOUT YOU BOSS?</h2>
+</body>
+</html>
