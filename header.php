608362cf895537489cff939109bac64d4bdf29a2
SAAKS6 <asfandyarnaeem81@gmail.com>
2023-11-16 18:01:18 +0500

    Header & Footer

---
+++ header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="page_width">
+        <div class="nav_desktop">
+            <div class="logo"><img src="./images/header/logo/logo.svg" alt="Company logo" /></div>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/service">Services</a></li>
+                    <li><a href="/contact-us">Contact Us</a></li>
+                    <li><a href="javascript:void(0)"><img src="./images/header/logo/user.png" height="20" alt="Call logo">
+                                <span>+971 58 67 47 123</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RIHAL</title>
+
+    <!-- INCLUDING PHP FILES -->
+    <!-- Explore Our Services  -->
+    <?php
+    include "./components/homepage/explore_our_services/service_card";
+
+    // Explore Our Services Images
+    $acServiceImg = "./images//home//explore_our_services//AC_Service_Card.png";
+    $ductCleaningServiceImg = "./images//home//explore_our_services//Duct_Cleaning_Card.png";
+    $handymanServiceImg = "./images//home//explore_our_services//Plumbing_Service_Card.png";
+    ?>
+
 </head>
+
 <body>
-    <h1>HOW ARE YOU BRO?</h1>
-    <h2>I AM GOOOOD, WHAT AbOUT YOU BOSS?</h2>
+<!-- HEADER -->
+    <?php
+    include "./header.php";
+     ?>
+
+<!-- EXPLORE OUR SERVICES -->
+    <section class="explore_our_services section_margin">
+        <div class="page_width">
+            <div class="explore_our_service_head">
+                <h2>Explore our <span>Services</span></h2>
+            </div>
+            <div class="explore_our_services_grid">
+
+                <?php
+                // Call the renderServicesCard function with different parameters
+                renderServicesCard($acServiceImg, "AC Service");
+                renderServicesCard($ductCleaningServiceImg, "Duct Cleaning");
+                renderServicesCard($handymanServiceImg, "Handyman");
+                // Add more calls for other services
+                ?>
+
+            </div>
+        </div>
+    </section>
+
+<!-- HEADER -->
+<?php
+    include "./footer.php";
+     ?>
 </body>
+
 </html>
